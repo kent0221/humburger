9e2ccfa3b997de7af0902d0eaeb80521facdf3ac
kent0221 <kent.mosstar@gmail.com>
2021-06-23 10:11:20 +0900

    sidebar

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 // Menuボタンを押すとサイドバーがフェードイン
 $(function () {
     $('.c-btn__menu').click(function () {
-        $('.l-grid__fixed, .l-sidebar').toggleClass('is-open');
+        $('.l-sidebar').toggleClass('is-open');
         $('.l-sidebar').fadeIn(500);
     });
 });
@@ -11,7 +11,7 @@ $(function () {
 // バツボタン押すとサイドバーがフェードアウト
 $(function () {
     $('.c-btn__close').click(function () {
-        $('.l-grid__fixed, .l-sidebar').toggleClass('is-open');
+        $('.l-sidebar').toggleClass('is-open');
         $('.l-sidebar').fadeOut(500);
     });
 
